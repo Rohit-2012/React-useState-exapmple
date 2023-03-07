f805fe6bc4bd25a357c3e5882a319928ab1a5fc0
Rohit-2012 <rohit.kirti1219@gmail.com>
2023-03-07 09:47:25 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Child1 } from "./child";
+import { Child2 } from "./child";
+// import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const arr1 = [23, 34, "Hey", 98]
+  const [toggle, setToggle] = useState(false);
+  console.log(toggle);
+  function handleToggle() {
+    setToggle(!toggle);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {arr1.map((elem)=><ul><li>{elem}</li></ul>)}
+      
+      {toggle ? <h1>Code with Sachin sir</h1> : <h1>Hello World</h1>}
+      <Child1 />
+      <Child2 />
+      <button onClick={handleToggle}>Click</button>
     </div>
   );
 }
---
+++ src/child.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export function Child1() {
+  return (
+    <div>
+      <h1>Child1 Component</h1>
+    </div>
+  )
+}
+export function Child2() {
+  return (
+    <div>
+      <h1>Child2 Component</h1>
+    </div>
+  )
+}
+
+// export default Child
